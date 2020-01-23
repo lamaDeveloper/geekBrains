d4b7e573d239c82ac8fa6df3e15072a3501d819a
lamaDeveloper <lama.developer.ee@gmail.com>
2020-01-23 16:26:30 +0200

    commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    catalogUrl: '/catalogData.json',
+    products: [],
+    imgCatalog: 'https://placehold.it/200x150',
+    searchLine: '',
+    show: false
+  },
+  methods: {
+    getJson(url) {
+      return fetch(url)
+          .then(result => result.json())
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    addProduct(product) {
+      console.log(product.id_product);
+    },
+    filterGoods(searchLine){
+
+    },
+  },
+  mounted() {
+    console.log('mounted');
+    this.getJson(`${API + this.catalogUrl}`)
+        .then(data => {
+          for (let el of data) {
+            this.products.push(el);
+          }
+        });
+
+    setTimeout(() => this.$destroy(), 2000)
+  },
+});
